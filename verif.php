1b88abe6fca631443c638e90656a31a0523e2e6d
Rishavhello <30924123+Rishavhello@users.noreply.github.com>
2017-12-06 19:53:47 +0530

    Update verif.php

--- verif.php
+++ verif.php
@@ -9,7 +9,7 @@
 	
 		
 		
-		$dbstatus = mysqli_select_db($con,"attendance") or die("Database not found");;
+	$dbstatus = mysqli_select_db($con,"attendance") or die("Database not found");;
 		
 		
 		//////////////////////////////////////////// I'll send an ID, you just search for it in the table, and return the password with a name value pair
@@ -17,42 +17,41 @@
                 //fetch ID as namevaluepair parameter 'ID' into $ID
                 
         $ID = $_POST['ID']; //entered by student in login page
-       $pass = $_POST['password']; //entered by student        //not sending password to PHP
+        $pass = $_POST['password']; //entered by student //not sending password to PHP
+	$filename = "temp_store_status.txt";
+	$handle = fopen($filename,'w');
+	$val = 0;
+	fwrite($handle, $val);
 		
-		$sql = "select  * from student where roll = $ID "; 
+	$sql = "select  id, password from student where roll = $ID "; 
 		
         
         
-		$result = mysqli_query($con,$sql);
+	$result = mysqli_query($con,$sql);
         
         if(mysql_affected_rows($result) == 0)
         {
             die('ROLL NOT FOUND');
         }
         
-		$list = array();
+		
 		 
 		
-		while ($row = mysqli_fetch_array($result,MYSQL_ASSOC))        //used to fetch the ResultSet and store in array format.
+	while ($row = mysqli_fetch_array($result,MYSQL_ASSOC))        //used to fetch the ResultSet and store in array format.
+	{
+			
+			
+        	if( ($pass == $row['password']) && ($ID == $row['id']) )
 		{
-			
-			/*
-                        $name = $row['name'];
-			$email= $row['email_id'];
-			$mobile = $row['mobile_no'];
-			$address = $row['address'];
-                        */
-            $password=$row['password']; //----name of column in bold //password in database
-
-			if($pass != $password)	//since I've already done checking in android
-         //   {
-         //       die('ENTER CORRECT PASSWORD');
-         //   }
-			array_push($list, array("ID"=>$ID,"password"=>$password));
-				//$row = mysqli_fetch_array($result);
+				
+			$val = 1;
+			fwrite($handle, $val);
+			fclose($handle);
 		}
-			
-		echo json_encode(array("list"=>$list));
 
+			
+	}
+			
+		
 
 ?>
